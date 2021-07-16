0ead35433cb00ef1cd33bf73408d5dfbbae8dad8
Ciara Swann <cccswann@gmail.com>
2021-07-15 22:41:39 -0700

    Updates

--- src/mock/data.js
+++
@@ -1,8 +0,0 @@
-// ABOUT DATA
-export const aboutData = {
-    img: '../images/profile.jpg',
-    paragraphOne: '',
-    paragraphTwo: '',
-    paragraphThree: '',
-    resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
-  };
